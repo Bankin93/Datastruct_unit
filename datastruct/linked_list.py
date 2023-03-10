fb7404cdc044963355ea7f10817c7c2eb0a634e1
Bankin93 <igorkonov1993@gmail.com>
2023-03-10 17:24:45 +0300

    homework 16.1

--- datastruct/linked_list.py
+++ datastruct/linked_list.py
@@ -29,6 +29,31 @@ class LinkedList:
             self.last_node.next_node = new_node
             self.last_node = new_node
 
+    def to_list(self):
+        """возвращает список с данными, содержащимися
+         в односвязном списке LinkedList"""
+        ll_list = []
+        node = self.first_node
+        while node is not None:
+            ll_list.append(node.data)
+            node = node.next_node
+        return ll_list
+
+    def get_data_by_id(self, value: int):
+        """возвращает первый найденный в LinkedList
+        словарь с ключом id, значение которого
+        равно переданному в метод значению"""
+        node = self.first_node
+        while node is not None:
+            try:
+                if node.data['id'] == value:
+                    return node.data
+                node = node.next_node
+            except TypeError:
+                print('Данные не являются словарем или в словаре нет id.')
+                break
+        return None
+
     def print_ll(self):
         """Метод для вывода в консоль данных из односвязанного списка"""
         ll_string = ''
@@ -41,3 +66,38 @@ class LinkedList:
 
         ll_string += 'None'
         return ll_string
+
+#if __name__ == '__main__':
+    # ll = LinkedList()
+    #
+    # ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+    # ll.insert_at_end({'id': 2, 'username': 'mik.roz'})
+    # ll.insert_at_end({'id': 3, 'username': 'mosh_s'})
+    # ll.insert_beginning({'id': 0, 'username': 'serebro'})
+
+    # метод to_list()
+    # lst = ll.to_list()
+    # for item in lst: print(item)
+    # {'id': 0, 'username': 'serebro'}
+    # {'id': 1, 'username': 'lazzy508509'}
+    # {'id': 2, 'username': 'mik.roz'}
+    # {'id': 3, 'username': 'mosh_s'}
+
+    # get_data_by_id()
+    # user_data = ll.get_data_by_id(3)
+    # print(user_data)
+    #{'id': 3, 'username': 'mosh_s'}
+
+    # работа блока try/except
+    # ll = LinkedList()
+    # ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+    # ll.insert_at_end('idusername')
+    # ll.insert_at_end([1, 2, 3])
+    # ll.insert_at_end({'id': 2, 'username': 'mosh_s'})
+
+    #user_data = ll.print_ll()
+    #user_data = ll.get_data_by_id(2)
+    # Данные не являются словарем или в словаре нет id.
+    # Данные не являются словарем или в словаре нет id.
+    #print(user_data)
+    #{'id': 2, 'username': 'mosh_s'}
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -31,3 +31,23 @@ class TestLinkedList(unittest.TestCase):
         self.ll.insert_at_end({'id': 3})
         self.ll.insert_beginning({'id': 0})
         self.assertEqual(self.ll.print_ll(), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
+
+    def test_to_list(self):
+        self.assertEqual(self.ll.to_list(), [])
+        self.ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        self.ll.insert_at_end({'id': 2, 'username': 'mik.roz'})
+        self.ll.insert_at_end({'id': 3, 'username': 'mosh_s'})
+        self.ll.insert_beginning({'id': 0, 'username': 'serebro'})
+        self.assertEqual(self.ll.to_list(), [{'id': 0, 'username': 'serebro'},
+                                             {'id': 1, 'username': 'lazzy508509'},
+                                             {'id': 2, 'username': 'mik.roz'},
+                                             {'id': 3, 'username': 'mosh_s'}])
+
+    def test_get_data_by_id(self):
+        self.assertEqual(self.ll.get_data_by_id(0), None)
+        self.ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        self.ll.insert_at_end('idusername')
+        self.ll.insert_at_end([1, 2, 3])
+        self.ll.insert_at_end({'id': 2, 'username': 'mosh_s'})
+        self.assertEqual(self.ll.get_data_by_id(1), {'id': 1, 'username': 'lazzy508509'})
+        self.assertEqual(self.ll.get_data_by_id(2), None)
